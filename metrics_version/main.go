bca8409eb8ea7f1fea4fa377a5262ed15ff2f714
SummerStar666 <50078285+SummerStar666@users.noreply.github.com>
2020-06-20 16:12:15 +0800

    Add files via upload

---
+++ metrics_version/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"example/metrics"
+)
+
+func main(){
+	http.HandleFunc("/abc", index)
+	http.Handle("/metrics", promhttp.Handler())
+	metrics.Register()
+	err := http.ListenAndServe(":5565", nil) // 设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	timer:=metrics.NewAdmissionLatency()
+	metrics.RequestIncrease()
+	num:=os.Getenv("Num")
+	if num==""{
+		Compute(6)
+		_,err:=w.Write([]byte("there is no env Num. Computation successed\n"))
+		if err!=nil{
+			log.Println("err:"+err.Error()+" No\n")
+		}
+	}else{
+		numInt,_:=strconv.Atoi(num)
+		Compute(numInt)
+		_,err:=w.Write([]byte("there is env Num. Computation successed\n"))
+		if err!=nil{
+			log.Println("err:"+err.Error()+" Yes\n")
+		}
+	}
+	timer.Observe()
+}
+
+func Compute(n int)int{
+	if n < 8{
+		return n%5
+	}else{
+		return n + Compute(n-1)
+	}
+	
+}
